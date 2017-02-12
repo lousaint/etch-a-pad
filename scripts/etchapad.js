8dda2e54f9f45a09ee9516035e403d6edcbf9212
Louis F. St. Hilaire <lousaint@hotmail.com>
2017-02-11 18:39:08 -0800

    Add color and shade options and controls

--- scripts/etchapad.js
+++ scripts/etchapad.js
@@ -1,39 +1,82 @@
+randomColor = false;
+progressiveShading = false;
+gridSize = 16;
+
 $(document).ready(function() {
-  var gridSize = 16;
+  drawGrid();
 
-  drawGrid(gridSize);
-
-  $("#shakebutton").click(function() {
-    gridSize = prompt("How many squares?");
-    $(".square").remove();
-    $(".row").remove();
-    drawGrid(gridSize);
+  $("#randomcolorsinput").click(function() {
+    randomColor = !randomColor;
+    $("#shadinginput").prop("disabled", randomColor);
   })
+
+  $("#shadinginput").click(function() {
+    progressiveShading = !progressiveShading;
+  });
+
+  $("#shakebutton").click(reset);
+
+  $("#gridsizebutton").click(function() {
+    var gridSizeInput = $("#gridsizeinput").val();
+    if (parseInt(gridSizeInput) == NaN) {
+      window.alert("Please enter a number.")
+      $("gridsizeinput").val("");
+    }
+    else {
+      gridSize = parseInt(gridSizeInput);
+      reset();
+    }
+  });
 });
 
-function drawGrid(gridSize)
-{
+function reset() {
+  $("#container").effect("shake", "left", "10", "3");
+  $("#container").children().remove();
+  drawGrid();
+}
+
+function drawGrid() {
   var containerWidth = $("#container").width();
   var containerHeight = $("#container").height();
   var squareHeight = containerHeight / gridSize;
   var squareWidth = containerWidth / gridSize;
 
-  for (var row = 0; row < gridSize; row++)
+  for (var squareCount = 0; squareCount < gridSize * gridSize; squareCount++)
   {
-    var $rowDiv = $('<div class="row" id="row' + row + '"></div>');
-    $("#container").append($rowDiv);
-    for (var column = 0; column < gridSize; column++)
-    {
-      var $columnDiv = $('<div class="square" id="square' + row + '-' + column + '"></div>');
-      $rowDiv.append($columnDiv);
-    }
+    var $squareDiv = $('<div class="square"></div>');
+    $("#container").append($squareDiv);
   }
 
-  $(".row").height(squareHeight);
-  $(".square").height(squareHeight);
-  $(".square").width(squareWidth);
+  $(".square").css({ "height" : squareHeight + "px",
+                      "width": squareWidth + "px"});
+  $("#container").children('div').mouseover(paintPixel);
+}
 
-  $(".square").hover(function() {
-    $(this).addClass("highlight");
-  });
+function paintPixel() {
+  if (randomColor) {
+    $(this).css("background-color", randomRGB());
+  }
+  else if (progressiveShading) {
+    var newRGBValue = darkenRGB($(this).css("background-color"));
+    $(this).css("background-color", newRGBValue);
+  }
+  else {
+    $(this).css("background-color", "black");
+  }
+}
+
+function randomRGB() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+
+  return "rgb(" + r + ", " + g + ", " + b + ")"
+}
+
+function darkenRGB(rgb) {
+  var rgbValue = parseInt(/[0-9]+/.exec(rgb)) - 20;
+  if (rgbValue < 0)
+    rgbValue = 0;
+
+  return "rgb(" + rgbValue + ", " + rgbValue + ", " + rgbValue + ")";
 }
